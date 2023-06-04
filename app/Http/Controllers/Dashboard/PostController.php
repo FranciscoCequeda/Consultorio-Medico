6358d4bd21936ff0bf1e33a278794b8f6455bf54
franciscoluis.cequeda.contractor <franciscoluis.cequeda.contractor.cidentity@bbva.com>
2023-06-04 01:50:36 -0500

    Adding Auth Middleware to Post & Category Controllers

--- app/Http/Controllers/Dashboard/PostController.php
+++ app/Http/Controllers/Dashboard/PostController.php
@@ -12,6 +12,16 @@ use Illuminate\Support\Facades\Gate;
 class PostController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
      * Display a listing of the resource.
      */
     public function index()
